40112f5a869fc13fa1099dfcad8cb969f3e0b68d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-09 23:56:45 -0300

    Removing Hello example

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,12 +3,6 @@ const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-routes.get('/', (request, result) => {
-    return result.json({
-        message: `Hello ${request.query.name}`
-    });
-});
-
 routes.post('/devs', DevController.store);
 
 module.exports = routes;
