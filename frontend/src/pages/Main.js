88d1e0d77eb121bc556f8a2d6e0c8ea4cf4ea831
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-11 15:01:35 -0300

    Implementing Main page with mocked data

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,3 +1,48 @@
-export default function Main() {
-    return <h1>Hello World</h1>
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres.</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Programador e cantor sertanejo nos tempos livres.</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
 }
