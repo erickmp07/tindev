b9e0c923add2466241464024a483e82df83cc2b0
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-11 18:13:14 -0300

    Fixing server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv:...', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
