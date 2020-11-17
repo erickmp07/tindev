2178975a4d0d34f18672a3b16f176da13dcfcb4e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-17 00:20:39 -0300

    Removing async storage manual link

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new AsyncStoragePackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
