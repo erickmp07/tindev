6d59d4273626451196d4318a139fe28551e69ce0
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-07-25 15:12:31 -0300

    Emit match to socket

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -14,15 +14,26 @@ module.exports = {
             });
         }
 
-        loggedDev.likes.push(targetDev._id);
-
-        await loggedDev.save();
-
         console.log(`User ${loggedDev.user} liked ${targetDev.user}.`);
 
         if (targetDev.likes.includes(loggedDev._id)) {
+            const loggedSocket = request.connectedUsers[user];
+            const targetSocket = request.connectedUsers[devId];
+
+            if (loggedSocket) {
+                request.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                request.io.to(targetSocket).emit('match', loggedDev);
+            }
+
             console.log('You got a Match!');
         }
+        
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
 
         return response.json(loggedDev);
     }
